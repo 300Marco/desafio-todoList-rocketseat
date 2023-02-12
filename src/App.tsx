0b3f8f77903c267a6eac2d42f4d3468ed7a255a7
Marco Santana <marcos.maio96@gmail.com>
2023-02-12 09:20:00 -0300

    Application App File

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './global.css';
+import styles from './components/App.module.css';
+
+import { Header } from './components/Header';
+import { AddTasks } from './components/AddTasks';
+import { Tasks } from './components/Tasks';
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <AddTasks />
+
+        <main>
+          <Tasks />
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
